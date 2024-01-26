0df54fcf1b694a68c18e30de34d9434ee6a8371b
ioangheraszim <gijohny17@gmail.com>
2024-01-26 22:18:00 +0000

    made some changes to the navbar

--- src/assets/components/Navbar/Navbar.jsx
+++ src/assets/components/Navbar/Navbar.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
-
+import logoIcon from "../../images/logo.svg"
+import closeIcon from "../../images/icon-close.svg"
+import iconHamburger from "../../images/icon-hamburger.svg"
+import "./Navbar.scss"
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <nav className='navbar'>
+      <img src={logoIcon} alt="" />
+      <ul className='navbar-items active'>
+        <li><a href="">Pricing</a></li>
+        <li><a href="">Product</a></li>
+        <li><a href="">About Us</a></li>
+        <li><a href="">Careers</a></li>
+        <li><a href="">Community</a></li>
+      </ul>
+      <button className='btn-start'>Get Started</button>
+      <button className='toggle-menu'>
+        <img src={iconHamburger} alt={iconHamburger} />
+      </button>
+    </nav>
   )
 }
 
