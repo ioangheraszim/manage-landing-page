fc9431c61c48b214f1576756342c1148084b8b93
ioangheraszim <gijohny17@gmail.com>
2024-02-27 21:56:10 +0000

    added data to testimonial data, and mapped the data

--- src/assets/components/Testimonials/Testimonials.jsx
+++ src/assets/components/Testimonials/Testimonials.jsx
@@ -1,46 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Testimonials.scss";
+import testimonialdata from "../../data/testimonialdata";
 import ali from "../../images/avatar-ali.png";
 
 function Testimonials() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleNextSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % testimonialdata.length);
+  };
+
   return (
     <section className="slider-wrapper">
       <h2>What the've said</h2>
-      <div className="slider-card">
-        <div className="slider">
-          <div className="the-image">
-            <img src={ali} alt={ali} />
+      <div className="slider-container">
+        {testimonialdata.map((slide, index) => (
+          <div
+            key={index}
+            className={`slider ${index === currentIndex ? "active" : ""}`}
+          >
+            <div className="the-image">
+              <img src={slide.image} alt={slide.name} />
+            </div>
+            <p className="name">{slide.name}</p>
+            <p className="comment">"{slide.comment}"</p>
           </div>
-          <p className="name">Ali Bravo</p>
-          <p className="comment">
-            "We have been able to cancel so many other subscriptions since using
-            Manage. There is no more cross-channel confusion and everyone is much
-            more focused."
-          </p>
-        </div>
-        <div className="slider">
-          <div className="the-image">
-            <img src={ali} alt={ali} />
-          </div>
-          <p className="name">Ali Bravo</p>
-          <p className="comment">
-            "We have been able to cancel so many other subscriptions since using
-            Manage. There is no more cross-channel confusion and everyone is much
-            more focused."
-          </p>
-        </div>
-        <div className="slider">
-          <div className="the-image">
-            <img src={ali} alt={ali} />
-          </div>
-          <p className="name">Ali Bravo</p>
-          <p className="comment">
-            "We have been able to cancel so many other subscriptions since using
-            Manage. There is no more cross-channel confusion and everyone is much
-            more focused."
-          </p>
-        </div>
-
+        ))}
       </div>
       <button className="btn-start">Get Started</button>
     </section>
