539ee1cbc5a25650f4c6ec08ad18c9d38758ec55
ioangheraszim <gijohny17@gmail.com>
2024-02-28 23:58:22 +0000

    finished carousel

--- src/assets/components/Testimonials/Testimonials.jsx
+++ src/assets/components/Testimonials/Testimonials.jsx
@@ -1,32 +1,46 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Testimonials.scss";
 import testimonialdata from "../../data/testimonialdata";
-import ali from "../../images/avatar-ali.png";
 
 function Testimonials() {
-  const [currentIndex, setCurrentIndex] = useState(0);
+  const [currentIndex, setCurrentIndex] = useState(testimonialdata.length - 1);
+
+  const showSlide = (index) => {
+    setCurrentIndex(index)
+  }
 
   const handleNextSlide = () => {
     setCurrentIndex((prevIndex) => (prevIndex + 1) % testimonialdata.length);
   };
 
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      handleNextSlide();
+    }, 3000);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [currentIndex]);
+
   return (
     <section className="slider-wrapper">
       <h2>What the've said</h2>
-      <div className="slider-container">
-        {testimonialdata.map((slide, index) => (
-          <div
-            key={index}
-            className={`slider ${index === currentIndex ? "active" : ""}`}
-          >
-            <div className="the-image">
-              <img src={slide.image} alt={slide.name} />
+        <div className="slider-container">
+          {testimonialdata.map((testimonial, index) => (
+            <div 
+              key={index}
+              className={`slider ${index === currentIndex ? "active" : ""}`}
+              style={{transform:`translateX(-${currentIndex * 100}%)`}}
+            >
+              <div className="the-image">
+                <img src={testimonial.image} alt={testimonial.name} />
+              </div>
+              <p className="name">{testimonial.name}</p>
+              <p className="comment">"{testimonial.comment}"</p>
             </div>
-            <p className="name">{slide.name}</p>
-            <p className="comment">"{slide.comment}"</p>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
       <button className="btn-start">Get Started</button>
     </section>
   );
